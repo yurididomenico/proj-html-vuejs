fcb3702da4b1ac02173a466e9d7b5a878c3aecf1
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-02 16:43:51 +0100

    Header: Struttura

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,42 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+//reset
+*
+{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  color: inherit;
+  text-decoration: none;
+  list-style-type: none;
+  font-family: 'Montserrat', sans-serif;
+}
+////////// GENERALE ////////////////////////////////////////////////////////////
+.contenitore
+{
+  width: 60%;
+  margin: auto;
+}
+////////// FLEX ////////////////////////////////////////////////////////////
+.f-mode
+{
+  display: flex;
+}
+.f-justify-center
+{
+  justify-content: center;
+}
+.f-justify-between
+{
+  justify-content: space-between;
+}
+.f-align-center
+{
+  align-items: center;
+}
+
+
 
 </style>
--- src/components/ComponenteHeader.vue
+++ src/components/ComponenteHeader.vue
@@ -1,6 +1,25 @@
 <template>
   <header>
-    HEADER
+    <!-- div per dare un margine automatico a destra e a sinistra -->
+    <div class="contenitore f-mode f-justify-between f-align-center">
+      <!-- Lista contatti navbar -->
+      <ul class="f-mode">
+        <li><span>[]</span> 1.800.567.8910 <span>24hrs</span></li>
+        
+        <li><span>[]</span>Contact Us</li>
+        
+        <li><span>[]</span>Alumns</li>
+        
+        <li><span>[]</span>Campus</li>
+      </ul>
+      <!-- Lista social navbar -->
+      <ul class="f-mode">
+        <li>[]</li>
+        <li>[]</li>
+        <li>[]</li>
+        <li>[]</li>
+      </ul>
+    </div>
   </header>
 </template>
 
@@ -13,5 +32,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+header
+{
+  background-color: #ffab0b;
+  color: white;
+  padding: 15px;
+  li
+  {
+    margin-right: 20px;
+    span
+    {
+      margin-right: 10px;
+    }
+  }
+}
 </style>
