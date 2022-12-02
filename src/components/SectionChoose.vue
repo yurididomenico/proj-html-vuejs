892138d18058c74059b037870693daaacef972b1
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-02 20:21:20 +0100

    Choose: Struttura bottom

--- src/components/SectionChoose.vue
+++ src/components/SectionChoose.vue
@@ -6,6 +6,19 @@
       <p class="sotto_titolo">We offer a wide range of high quality of teaching and extra-curricular activities.</p>
       <div class="linea"></div> 
     </div>
+
+    <div class="contenitore f-mode choose">
+      <div class="chooseText">
+        <span>titolo</span>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi perspiciatis mollitia aliquam delectus repellendus distinctio reiciendis, officia, earum ipsum veritatis enim nobis laborum laboriosam similique sequi accusamus velit, cum modi.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi perspiciatis mollitia aliquam delectus repellendus distinctio reiciendis, officia, earum ipsum veritatis enim nobis laborum laboriosam similique sequi accusamus velit, cum modi.</p>
+        </div>
+      </div>
+      <div class="chooseImg">
+        <img src="../assets/img/University_img.jpg" alt="">
+      </div>
+    </div>
   </section>
 </template>
 
@@ -21,6 +34,10 @@ section
 {
   padding: 100px 0;
   text-align: center;
+  div
+  {
+    margin-bottom: 100px;
+  }
   *
   {
     margin-bottom: 30px;
@@ -41,6 +58,33 @@ section
   {
     font-size: 1.2rem;
   }
+
+  .chooseText
+  {
+    text-align: left;
+    width: 130%;
+    padding-right: 50px;
+    
+    span
+    {
+      font-size: 2rem;
+      font-weight: bold;
+    }
+    div
+    {
+      line-height: 1.8rem;
+    }    
+  }
+  .chooseImg
+  {
+    width: 70%;
+    padding-top: 40px;
+    img
+    {
+      width: 100%;
+    }
+  }
+  
 }
 
 </style>
