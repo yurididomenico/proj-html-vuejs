f9ee84fd5de63f2410f9f60164e689faf92f5dc6
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-02 15:18:14 +0100

    Set Workspace

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* add icons to the library */
+library.add(faHatWizard)
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
