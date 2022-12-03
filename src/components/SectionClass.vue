7a6667136d359b1032b81e583fecf2f2162cb7dc
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-03 14:22:12 +0100

    Class: Compilazione e Fix margini

--- src/components/SectionClass.vue
+++ src/components/SectionClass.vue
@@ -1,51 +1,48 @@
 <template>
   <section>
-    <div class="contenitore f-mode">
+    <div class="contenitore f-mode f-justify-between">
       <div class="class-facilities">
-        <h2>TITOLO</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illum, quaerat minima aliquid molestias officia ex cumque ea labore accusantium harum.</p>
-        <div>
-          <div class="f-mode">
-            <span>[]</span>
-            <h3>titolino</h3>
-            <p>testo s s  sssssss ffff ssss </p>
+        <h2>WORLD CLASS FACILITIES</h2>
+        <p>Avada University is devoted to excellence in teaching, learning, and research, and developing leaders in mani disciplines who make a difference globally.</p>
+        <div class="facilities-middle">
+          <div class="f-mode f-align-center">
+            <font-awesome-icon icon="fa-solid fa-globe" />
+            <h3>International focus <span>- our students come from across the globe</span></h3>
           </div>
-          <div class="f-mode">
-            <span>[]</span>
-            <h3>titolino</h3>
-            <p>testo s s  sssssss ffff ssss </p>
+          <div class="f-mode f-align-center">
+            <font-awesome-icon icon="fa-solid fa-graduation-cap" />
+            <h3 class="test">Residential courses <span>- we encourage learning and growth</span></h3>
           </div>
-          <div class="f-mode">
-            <span>[]</span>
-            <h3>titolino</h3>
-            <p>testo s s  sssssss ffff ssss </p>
+          <div class="f-mode f-align-center">
+            <font-awesome-icon icon="fa-solid fa-address-book" />
+            <h3>People not profits <span>- education is a right not a privilege</span></h3>
           </div>
         </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illum, quaerat minima aliquid molestias officia ex cumque ea labore accusantium harum.</p>
+        <p>If you prefer to study in your own time and at your own speed then on of our e-learning packages may be right up your street.</p>
       </div>
 
       <div class="class-values">
-        <h2>TITOLO</h2>
-        <div class="f-mode">
-          <span>[]</span>
-          <h3>testo testo</h3>
-        </div>
-        <div class="f-mode">
-          <span>[]</span>
-          <h3>testo testo</h3>
-        </div>
-        <div class="f-mode">
-          <span>[]</span>
-          <h3>testo testo</h3>
-        </div>
-        <div class="f-mode">
-          <span>[]</span>
-          <h3>testo testo</h3>
-        </div>
-        <div class="f-mode">
-          <span>[]</span>
-          <h3>testo testo</h3>
-        </div>
+        <h2>OUR VALUES</h2>
+        <a href="#">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+          <h3>OUR MISSION</h3>
+        </a>
+        <a href="#">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+          <h3>OUR PHILOSOPHY</h3>
+        </a>
+        <a href="#">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+          <h3>OUR COMMITMENT</h3>
+        </a>
+        <a href="#">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+          <h3>OUR PROMISE</h3>
+        </a>
+        <a href="#">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+          <h3>OUR VALUES</h3>
+        </a>
       </div>
     </div>
   </section>
@@ -62,9 +59,69 @@ section
 {
   background-color: #f7f5f4;
   padding: 100px 0;
+
+
   .class-facilities
   {
-    flex-basis: 50%;
+    flex-basis: 55%;
+    padding-right: 30px;
+
+    >*
+    {
+      margin-bottom: 30px;
+    }
+    .facilities-middle
+    {
+      span
+      {
+        color: #4a4b4b;
+        font-weight: normal;
+      }
+      div
+      {
+        margin-bottom: 20px;
+        
+        h3
+        {
+          margin-left: 10px;
+        }
+        .test
+        {
+          margin-left: 5px;
+        }
+      }
+    }
+  }
+
+
+  .class-values
+  {
+    flex-basis: 40%;
+    font-weight: bold;
+
+    h2
+    {
+      margin-bottom: 20px;
+    }
+
+    a
+    {
+      display: flex;
+      align-items: center;
+      padding: 15px;
+      background-color: white;
+      margin-bottom: 10px;
+      margin-left: 20px;
+      color: #ffab0b;
+      
+
+      h3
+      {
+        font-size: 0.9rem;
+        margin-left: 20px;
+      }
+    }
+
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import { faPhoneFlip, faEnvelope, faGraduationCap, faBuildingColumns, faPlay, faSignal, faGear, faMaximize } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneFlip, faEnvelope, faGraduationCap, faBuildingColumns, faPlay, faSignal, faGear, faMaximize, faGlobe, faAddressBook, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF, faTwitter, faYoutube, faInstagram} from '@fortawesome/free-brands-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* add icons to the library */
 library.add(
-  faPhoneFlip, faEnvelope, faGraduationCap, faBuildingColumns, faPlay, faSignal, faGear, faMaximize,
+  faPhoneFlip, faEnvelope, faGraduationCap, faBuildingColumns, faPlay, faSignal, faGear, faMaximize, faGlobe, faAddressBook, faPlus,
   faFacebookF, faTwitter, faYoutube, faInstagram
 )
 /* add font awesome icon component */
