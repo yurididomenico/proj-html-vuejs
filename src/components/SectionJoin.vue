7093b6163fd1cf1ebaaef1f880f740a9cd5b2806
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-03 17:06:22 +0100

    Join: Compilazione e cambio colore

--- src/components/SectionJoin.vue
+++ src/components/SectionJoin.vue
@@ -1,14 +1,16 @@
 <template>
   <section>
-    <h2>TITOLONE</h2>
-    <p>titoletto</p>
-    <form>
-      <input type="text" placeholder="Name">
-      <input type="email" placeholder="Email">
-      <input type="number" placeholder="Telephone">
-      <a href="#">APPLY NOW</a>
+    <div class="contenitore">
+      <h2>Joining Over <span>500,000 Students </span>Enjoying Avada Education now</h2>
+      <p>Become Part of Avada University to Further Your Career</p>
+      <form>
+        <input type="text" placeholder="Name">
+        <input type="email" placeholder="Email">
+        <input type="number" placeholder="Telephone">
+        <a href="#">APPLY NOW</a>
 
-    </form>
+      </form>
+    </div>
   </section>
 </template>
 
@@ -21,6 +23,52 @@ export default {
 <style scoped lang="scss">
 section
 {
+  background-color: #333c4e;
   text-align: center;
+  padding: 100px 0;
+
+  h2
+  {
+    color: white;
+    font-size: 2rem;
+    span
+    {
+      color: #ffab0b;
+    }
+  }
+
+  p
+  {
+    color: grey;
+    font-size: 1.2rem;
+    margin-bottom: 60px;
+  }
+
+  form
+  {
+    
+    input
+    {
+      background-color: #444d63;
+      
+      width: calc(100% / 5 - 10px);
+      border: none;
+      padding: 10px;
+      margin: 0 10px;
+    }
+    a
+    {
+      background-color: #f09a3f;
+      color: white;
+      padding: 10px 50px;
+      width: calc(100% / 4);
+      font-size: 0.8rem;
+      font-weight: bold;
+    }
+    input::placeholder
+    {
+      color: white;
+    }
+  }
 }
 </style>
